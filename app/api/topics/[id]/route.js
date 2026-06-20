dc5138e9063f5beaf765b6e7b39de38063c7ece4
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-06-19 23:46:33 -0400

    full CRUD functionality working in Thunder Client

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -5,16 +5,12 @@ import { NextResponse } from "next/server"
 export async function PUT(req,{params}){
     try{
         await connectMongoDB()
-
         const {id} = await params
-
         const body = await req.json()
-
         await Topic.findByIdAndUpdate(
             id,
             body
         )
-
         return NextResponse.json({
             message:"Topic Updated"
         })
